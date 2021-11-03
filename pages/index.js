a441b45f981f2e395eea7f58c8c03d1b91f969ad
Ryan Duffy <ryan@replay.io>
2021-11-03 09:43:24 -0700

    Remove website code and add deployment action for demo (#160)
    * Remove website code and add deployment action for demo
    
    * Use a regular deploy workflow instead of a custom GitHub action
    
    Co-authored-by: Dan Miller <dan@replay.io>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import Hero from "./components/Hero";
-import Testimonials from "./components/Testimonials";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 export default function Home() {
   return (
@@ -12,36 +7,12 @@ export default function Home() {
         <script
           dangerouslySetInnerHTML={{
             __html: ` (function () {
-            var qs,
-              js,
-              q,
-              s,
-              d = document,
-              gi = d.getElementById,
-              ce = d.createElement,
-              gt = d.getElementsByTagName,
-              id = "typef_orm_share",
-              b = "https://embed.typeform.com/";
-            if (!gi.call(d, id)) {
-              js = ce.call(d, "script");
-              js.id = id;
-              js.src = b + "embed.js";
-              q = gt.call(d, "script")[0];
-              q.parentNode.insertBefore(js, q);
-            }
+            document.location.replace('./demo/index.html');
           })();`
           }}
         />
       </Head>
-      <div className="space-y-8">
-        <div className="bg-gray-50">
-          <Header />
-          <Hero />
-        </div>
-        <Features />
-        <Testimonials />
-        <Footer />
-      </div>
+      <a href="/demo/index.html">Replay Demo</a>
     </>
   );
 }
