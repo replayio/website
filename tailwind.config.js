baf0d0f51c281b3122cd686ee621c27ab2712f68
Josh Morrow <josh@jcmorrow.com>
2024-05-02 08:38:10 -0700

    Remove breaking font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      // Removing `BlinkMacSystemFont, ui-sans-serif, system-ui` for https://linear.app/replay/issue/TT-1070/remove-breaking-font-from-firstreplayio
+      sans: "-apple-system,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji"
+    }
   },
   plugins: []
 };
